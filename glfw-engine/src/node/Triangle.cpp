48a14d40cc752eaab4d462d0424d484c7ce36e70
lzpel <18492524+lzpel@users.noreply.github.com>
2020-11-15 23:28:04 +0900

    Replace glBegin~glEnd

--- glfw-engine/src/node/Triangle.cpp
+++ glfw-engine/src/node/Triangle.cpp
@@ -8,12 +8,22 @@ void Triangle::Init() {
 }
 void Triangle::Draw() {
 	Stat& w=NodeState("WINDOW");
-	glBegin(GL_TRIANGLES);
-	glColor3f(0.5+0.5*sin(5*M_PI*w.time/w.frq), 0.5+0.5*sin(7*M_PI*w.time/w.frq), 0.5+0.5*sin(11*M_PI*w.time/w.frq));
-	glVertex2f(sin(2*M_PI*w.time/w.frq), sqrt(3));
-	glVertex2f(-1, 0);
-	glVertex2f(1, 0);
-	glEnd();
+	GLfloat m[18]={0};
+	m[0]=sin(2*M_PI*w.time/w.frq);
+	m[1]=sqrt(3);
+	m[3]=0.5+0.5*sin(5*M_PI*w.time/w.frq);
+	m[4]=0.5+0.5*sin(7*M_PI*w.time/w.frq);
+	m[5]=0.5+0.5*sin(11*M_PI*w.time/w.frq);
+	m[06]=+1;
+	m[12]=-1;
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_COLOR_ARRAY);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glVertexPointer(3, GL_FLOAT, sizeof(GLfloat)*6, &m[0]);
+	glColorPointer(3,GL_FLOAT,sizeof(GLfloat)*6, &m[3]);
+	glDrawArrays(GL_TRIANGLES,0,3);
+	glDisableClientState(GL_VERTEX_ARRAY);
+	glDisableClientState(GL_COLOR_ARRAY);
 }
 void Triangle::Terminate() {
 
